9acfa233e12716745913cd6fafaae24f6d80dfaf
lmxchyy <lmxchyy@gmail.com>
2022-11-03 01:17:10 +0800

    Second commit

--- components/alert.js
+++ components/alert.js
@@ -30,7 +30,7 @@ export default function Alert({ preview }) {
                 href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                 className="underline hover:text-success duration-200 transition-colors"
               >
-                available on GitHub
+                available on GitHub web
               </a>
               .
             </>
